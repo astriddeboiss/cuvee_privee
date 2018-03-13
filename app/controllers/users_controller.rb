949f084198907f2badee1577a5948bbceb8ac101
Astrid de Boissieu <astrid.de-boissieu@hec.edu>
2018-03-13 14:01:53 +0100

    Added links between pages and signout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:new, :create]
+
   def new
   end
 
   def create
+  end
+
+  def dashboard
   end
 end
