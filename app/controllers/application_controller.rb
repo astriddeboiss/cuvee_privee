18a72fa13360805c3210a720e4eda469f24d00d3
Astrid de Boissieu <astrid.de-boissieu@hec.edu>
2018-03-15 11:02:37 +0100

    Added first and last name to the sign in form in ordre to have the info in the dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!,  :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:picture, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:picture, :username])
   end
   private
 
