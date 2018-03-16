c9883881adc5777e3fb623e80157c94fb321ec31
Marie-Lilas Onanga-Ozavino <marie-lilas.onanga-ozavino@hec.edu>
2018-03-16 16:03:40 +0100

    fixed bugs

--- app/javascript/components/filter.js
+++ app/javascript/components/filter.js
@@ -2,13 +2,13 @@ function submitFormOnSelectChange() {
   const select = document.getElementById('variety-tag');
   if (select) {
     select.addEventListener("change", (event) => {
-      event.currentTarget.parentElement.submit();
+      event.currentTarget.parentElement.parentElement.submit();
     })
   }
   const select_price = document.getElementById('price-tag');
   if (select_price) {
     select.addEventListener("change", (event) => {
-      event.currentTarget.parentElement.submit();
+      event.currentTarget.parentElement.parentElement.submit();
     })
   }
 }
